b1c1502e9369b3ab0077c7d6f57459d936fa0719
Bzuzinho <ricardo.jgvf@gmail.com>
2026-08-13 22:54:30 +0100

    Desportivo Registos: manter gate de leitura compatível

--- routes/desportivo_records.php
+++ routes/desportivo_records.php
@@ -8,11 +8,11 @@ Route::middleware(['web','auth','verified','module.access:desportivo'])
     ->name('desportivo.registos.')
     ->group(function (): void {
         Route::get('/', [SportsRecordsWorkspaceController::class,'index'])
-            ->middleware('permission.access:desportivo.registos,view')->name('index');
+            ->middleware('permission.access:desportivo.treinos.cais,view')->name('index');
         Route::get('/export', [SportsRecordsWorkspaceController::class,'export'])
-            ->middleware('permission.access:desportivo.registos,view')->name('export');
+            ->middleware('permission.access:desportivo.treinos.cais,view')->name('export');
         Route::get('/treinos/{training}', [SportsRecordsWorkspaceController::class,'training'])
-            ->middleware('permission.access:desportivo.registos,view')->name('training');
+            ->middleware('permission.access:desportivo.treinos.cais,view')->name('training');
         Route::get('/atletas/{athlete}', [SportsRecordsWorkspaceController::class,'athlete'])
-            ->middleware('permission.access:desportivo.registos,view')->name('athlete');
+            ->middleware('permission.access:desportivo.treinos.cais,view')->name('athlete');
     });
