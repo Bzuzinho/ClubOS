f716fd6e9bda9aa4e4112b96d41408fae47077e3
Bzuzinho <ricardo.jgvf@gmail.com>
2026-08-13 08:56:34 +0100

    Fix canonical planning route action representation

--- routes/desportivo_planning_canonical.php
+++ routes/desportivo_planning_canonical.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\Desportivo\SportsPlanningWorkspaceController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['web', 'auth', 'verified', 'module.access:desportivo'])
-    ->get('/desportivo/planeamento', [SportsPlanningWorkspaceController::class, 'index'])
+    ->get('/desportivo/planeamento', SportsPlanningWorkspaceController::class . '@index')
     ->middleware('permission.access:desportivo.planeamento,view')
     ->name('desportivo.planeamento');
