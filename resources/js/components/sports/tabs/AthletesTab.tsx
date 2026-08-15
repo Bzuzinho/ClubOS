4eb29abefe7768af6a2448ca87f0a4640c3dbbae
Bzuzinho <ricardo.jgvf@gmail.com>
2026-08-15 12:07:07 +0100

    Desportivo cleanup: remove legacy Athletes tab wrapper

--- resources/js/components/sports/tabs/AthletesTab.tsx
+++
@@ -1,8 +0,0 @@
-import type { ComponentProps } from 'react';
-import { DesportivoAtletasTab } from '@/Components/Desportivo/DesportivoAtletasTab';
-
-type AthletesTabProps = ComponentProps<typeof DesportivoAtletasTab>;
-
-export function AthletesTab(props: AthletesTabProps) {
-  return <DesportivoAtletasTab {...props} />;
-}
