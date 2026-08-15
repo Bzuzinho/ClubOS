54c804c0eb5c0582ca73c2310f037aa1684b6f98
Bzuzinho <ricardo.jgvf@gmail.com>
2026-08-15 12:07:46 +0100

    Desportivo cleanup: remove legacy Performance tab wrapper

--- resources/js/components/sports/tabs/PerformanceTab.tsx
+++
@@ -1,8 +0,0 @@
-import type { ComponentProps } from 'react';
-import { DesportivoPerformanceTab } from '@/Components/Desportivo/DesportivoPerformanceTab';
-
-type PerformanceTabProps = ComponentProps<typeof DesportivoPerformanceTab>;
-
-export function PerformanceTab(props: PerformanceTabProps) {
-  return <DesportivoPerformanceTab {...props} />;
-}
