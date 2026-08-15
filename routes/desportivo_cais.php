4d4b154f8a1ecdfc24a358f2371d7ad1b957e1c3
Bzuzinho <ricardo.jgvf@gmail.com>
2026-08-15 12:02:30 +0100

    Desportivo cleanup: register Cais metrics on canonical routes

--- routes/desportivo_cais.php
+++ routes/desportivo_cais.php
@@ -6,6 +6,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['web', 'auth', 'verified', 'module.access:desportivo'])
     ->prefix('desportivo/cais')
     ->group(function (): void {
+        Route::get('/metricas', [SportsCaisWorkspaceController::class, 'metrics'])
+            ->middleware('permission.access:desportivo.treinos.cais,view')
+            ->name('desportivo.cais.metrics.index');
+        Route::post('/metricas', [SportsCaisWorkspaceController::class, 'storeMetrics'])
+            ->middleware('permission.access:desportivo.treinos.cais,edit')
+            ->name('desportivo.cais.metrics.store');
         Route::patch('/{training}/atletas/{athlete}/presenca', [SportsCaisWorkspaceController::class, 'presence'])
             ->middleware('permission.access:desportivo.treinos.cais,edit')
             ->name('desportivo.cais.presence');
