fa55c1d9802b691f5d83a83318967d45b45ef384
Bzuzinho <ricardo.jgvf@gmail.com>
2026-08-22 23:39:05 +0100

    fix(types): declarar canais de comunicação

--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -2,6 +2,8 @@ import { AxiosInstance } from 'axios';
 import { route as ziggyRoute } from 'ziggy-js';
 
 declare global {
+    type Channel = 'email' | 'sms' | 'push' | 'interno' | 'alert_app';
+
     interface Window {
         axios: AxiosInstance;
     }
