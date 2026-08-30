cad38ad29bf02d3995fbf9316a85938436cffb72
Bzuzinho <ricardo.jgvf@gmail.com>
2026-08-30 14:54:20 +0100

    refactor(routing): modularize administrative logistics routes

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ use App\Http\Controllers\Communication\CommunicationCampaignController;
 use App\Http\Controllers\Communication\CommunicationDeliveryController;
 use App\Http\Controllers\Communication\CommunicationSegmentController;
 use App\Http\Controllers\Communication\CommunicationTemplateController;
-use App\Http\Controllers\LogisticaController;
 use App\Http\Controllers\EquipasController;
 use App\Http\Controllers\Financeiro\FiscalDocumentRequestController;
 use App\Http\Controllers\PublicFormSubmissionController;
@@ -99,26 +98,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     require __DIR__.'/web_finance.php';
 
-    Route::prefix('logistica')->middleware('module.access:logistica')->group(function () {
-        Route::get('/', [LogisticaController::class, 'index'])->name('logistica.index');
-        Route::post('/requisicoes', [LogisticaController::class, 'storeRequest'])->name('logistica.requisicoes.store');
-        Route::put('/requisicoes/{logisticsRequest}', [LogisticaController::class, 'updateRequest'])->name('logistica.requisicoes.update');
-        Route::delete('/requisicoes/{logisticsRequest}', [LogisticaController::class, 'destroyRequest'])->name('logistica.requisicoes.destroy');
-        Route::post('/requisicoes/{logisticsRequest}/aprovar', [LogisticaController::class, 'approveRequest'])->name('logistica.requisicoes.approve');
-        Route::post('/requisicoes/{logisticsRequest}/faturar', [LogisticaController::class, 'invoiceRequest'])->name('logistica.requisicoes.invoice');
-        Route::post('/requisicoes/{logisticsRequest}/entregar', [LogisticaController::class, 'deliverRequest'])->name('logistica.requisicoes.deliver');
+    require __DIR__.'/web_logistics.php';
 
-        Route::post('/stock/movimentos', [LogisticaController::class, 'registerStockMovement'])->name('logistica.stock.movimentos.store');
-
-        Route::post('/emprestimos', [LogisticaController::class, 'storeLoan'])->name('logistica.emprestimos.store');
-        Route::put('/emprestimos/{equipmentLoan}', [LogisticaController::class, 'updateLoan'])->name('logistica.emprestimos.update');
-        Route::delete('/emprestimos/{equipmentLoan}', [LogisticaController::class, 'destroyLoan'])->name('logistica.emprestimos.destroy');
-        Route::post('/emprestimos/{equipmentLoan}/devolver', [LogisticaController::class, 'returnLoan'])->name('logistica.emprestimos.return');
-
-        Route::post('/fornecedores/compras', [LogisticaController::class, 'registerSupplierPurchase'])->name('logistica.fornecedores.compras.store');
-        Route::put('/fornecedores/compras/{supplierPurchase}', [LogisticaController::class, 'updateSupplierPurchase'])->name('logistica.fornecedores.compras.update');
-        Route::delete('/fornecedores/compras/{supplierPurchase}', [LogisticaController::class, 'destroySupplierPurchase'])->name('logistica.fornecedores.compras.destroy');
-    });
     Route::post('financeiro/{financeiro}/apagar', [FinanceiroController::class, 'destroy'])
         ->middleware(['module.access:financeiro', 'permission.access:financeiro.dashboard,delete'])
         ->name('financeiro.destroy.post');
