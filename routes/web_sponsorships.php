21762168bb9694b75a6f382b9247b6bcbadde791
Bzuzinho <ricardo.jgvf@gmail.com>
2026-09-24 15:51:57 +0100

    P2.7: move sponsor CRUD routes to Patrocinios

--- routes/web_sponsorships.php
+++ routes/web_sponsorships.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\PatrocinosController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('patrocinios')->middleware('module.access:patrocinios')->group(function () {
+    Route::post('/patrocinadores', [PatrocinosController::class, 'storeSponsor'])->name('patrocinios.patrocinadores.store');
+    Route::put('/patrocinadores/{sponsor}', [PatrocinosController::class, 'updateSponsor'])->name('patrocinios.patrocinadores.update');
+    Route::delete('/patrocinadores/{sponsor}', [PatrocinosController::class, 'destroySponsor'])->name('patrocinios.patrocinadores.destroy');
     Route::get('/integracoes', [PatrocinosController::class, 'integrationsIndex'])->name('patrocinios.integrations.index');
     Route::post('/{patrocinio}/integracoes/retry', [PatrocinosController::class, 'retry'])->name('patrocinios.integrations.retry');
     Route::post('/{patrocinio}/fechar', [PatrocinosController::class, 'close'])->name('patrocinios.close');
